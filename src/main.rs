b847d257c2865234170cb69e18627b7040ad8ba2
Daniel Stoyak <dstoyak@gmail.com>
2024-01-25 15:00:45 -0800

    updated structs and started implementations

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,68 @@
+use ndarray
+use std::{env, f32};
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, Read, stdout, Write};
+use std::time::SystemTime;
+#[cfg(feature = "threads")]
+use rayon::prelude::*;
+
 struct Config {
-    dim: u64,
-    hidden_dim: u64,
-    n_layers: u64,
-    n_heads: u64,
-    n_kv_heads: u64,
-    vocab_size: u64,
-    seq_len: u64,
+    dim: usize,
+    hidden_dim: usize,
+    n_layers: usize,
+    n_heads: usize,
+    n_kv_heads: usize,
+    vocab_size: usize,
+    seq_len: usize,
+}
+
+impl Config() {
+    fn from_buf_reader(f: &mut BufReader<File) -> Self {
+        let c = Self {
+            dim: read::<i32>(f),
+            hidden_dim: read::<i32>(f),
+            n_layers: read::<i32>(f),
+            n_heads: read::<i32>(f),
+            n_kv_heads: read::<i32>(f),
+            seq_len: read::<i32>(f),
+            vocab_size: read::<i32>(f),
+            dim: read::<i32>(f),
+        };
+        Self {
+            vocab_size: c.vocab_size.abs(),
+            ..c
+        }
+    }
 }
 struct TransformerWeights {
-    token_embedding_table: f64,
-    rms_att_weight: f64,
-    rms_ffn_weight: f64,
-    wq: f64,
-    wk: f64,
-    wv: f64,
-    wo: f64,
-    w1: f64,
-    w2: f64,
-    w3: f64,
-    rms_fnal_weight: f64,
-    wcls: f64,
+    // token embedding table
+    token_embedding_table: Vec<f32>,  // (vocab_size, dim)
+    // weights for rmsnorms
+    rms_att_weight: Vec<f32>, // (layer, dim) rmsnorm weights
+    rms_ffn_weight: Vec<f32>, // (layer, dim)
+    // weights for matmuls
+    wq: Vec<f32>, // (layer, dim, dim)
+    wk: Vec<f32>, // (layer, dim, dim)
+    wv: Vec<f32>, // (layer, dim, dim)
+    wo: Vec<f32>, // (layer, dim, dim)
+    // weights for ffn
+    w1: Vec<f32>, // (layer, hidden_dim, dim)
+    w2: Vec<f32>, // (layer, dim, hidden_dim)
+    w3: Vec<f32>, // (layer, hidden_dim, dim)
+    // final rmsnorm
+    rms_final_weight: Vec<f32>, // (dim,)
+    // freq_cis for RoPE relatively positional embeddings
+    freq_cis_real: Vec<f32>, // (seq_len, dim/2)
+    freq_cis_imag: Vec<f32>, // (seq_len, dim/2)
+    // optional output embedding
+    wcls: Option<Vec<f32>>, // (vocab_size, dim)
 }
+
+impl TransformerWeights() {
+
+}
+
 struct RunState {
     x: f64,
     xb: f64,
